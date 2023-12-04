63d1142fb3dc545d9656e49c671c42ebe0fc81e3
n2ptune <n2ptune@outlook.com>
2023-12-05 00:47:55 +0900

    chore: switch color theme

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 const { $firebaseApp } = useNuxtApp()
+const colorMode = useColorMode()
+
+const iconName = computed(() => {
+  return colorMode.preference === 'dark' ? 'i-heroicons-sun' : 'i-heroicons-moon-solid'
+})
+
+function onClickChangePreference() {
+  colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'
+  colorMode.preference = colorMode.value
+}
 </script>
 
 <template>
@@ -7,6 +17,18 @@ const { $firebaseApp } = useNuxtApp()
     <div class="container mx-auto flex justify-between">
       <div class="text-2xl font-black">
         {{ 'CARPRO' }}
+      </div>
+      <div class="space-x-4">
+        <ClientOnly>
+          <UButton
+            :icon="iconName"
+            sqaure
+            variant="link"
+            size="xl"
+            color="black"
+            @click="onClickChangePreference"
+          />
+        </ClientOnly>
       </div>
     </div>
   </header>
