b0b49e4aea7f9b3489289349cd009f49f2c2109f
n2ptune <n2ptune@outlook.com>
2024-01-02 18:05:37 +0900

    feat: change required content in user profile

--- src/components/profile/item/BasicInformation.vue
+++ src/components/profile/item/BasicInformation.vue
@@ -9,22 +9,27 @@ interface Props {
 const props = defineProps<Props>()
 const userProfile = inject<Ref<UserProfile>>(profileSymbol) as Ref<UserProfile>
 const requiredError = reactive({
-  templateName: false,
-  name: false
+  // templateName: false,
+  name: false,
+  position: false
 })
 
 const validateForm = () => {
   const validates = [
+    // {
+    //   condition: () => userProfile.value.templateName,
+    //   requiredErrorKey: 'templateName'
+    // },
+    { condition: () => userProfile.value.name, requiredErrorKey: 'name' },
     {
-      condition: () => userProfile.value.templateName,
-      requiredErrorKey: 'templateName'
-    },
-    { condition: () => userProfile.value.name, requiredErrorKey: 'name' }
+      condition: () => userProfile.value.position,
+      requiredErrorKey: 'position'
+    }
   ]
 
   const validated = validates.every((valid) => {
     if (!valid.condition()) {
-      requiredError[valid.requiredErrorKey as 'templateName' | 'name'] = true
+      requiredError[valid.requiredErrorKey as 'name' | 'position'] = true
     }
     return valid.condition()
   })
@@ -40,28 +45,18 @@ defineExpose({
 <template>
   <ProfileItemBase :label="props.tab.name">
     <div class="space-y-4">
-      <UFormGroup
-        label="프로필 템플릿 제목"
-        help="프로필 템플릿 제목은 템플릿 가장 상단에 가장 큰 글씨로 표현되는 항목입니다."
-        required
-        :error="
-          requiredError.templateName
-            ? '프로필 템플릿 제목은 필수 항목입니다.'
-            : ''
-        "
-      >
+      <UFormGroup label="프로필 템플릿 제목">
         <UInput
           v-model="userProfile.templateName"
           placeholder="프로필 템플릿 제목을 입력하세요."
           size="xl"
           :maxlength="20"
-          @update:model-value="() => (requiredError.templateName = false)"
         />
       </UFormGroup>
 
       <UFormGroup
         label="이름"
-        help="이름은 프로필에서 필수 항목입니다."
+        help="이름은 기본 정보에서 필수 항목입니다."
         :error="requiredError.name ? '이름은 필수 항목입니다.' : ''"
         required
       >
@@ -83,22 +78,28 @@ defineExpose({
         />
       </UFormGroup>
 
-      <UFormGroup label="직무" help="프로필에 들어갈 포지션을 적어주세요.">
+      <UFormGroup
+        label="직무"
+        help="프로필에 표시할 직무를 입력하세요."
+        required
+        :error="requiredError.position ? '직무는 필수 항목입니다.' : ''"
+      >
         <UInput
           v-model="userProfile.position"
           placeholder="프론트엔드 개발자"
           size="xl"
           :maxlength="20"
+          @update:model-value="() => (requiredError.position = false)"
         />
       </UFormGroup>
 
       <UFormGroup
-        label="나를 소개하는 짧은 글"
+        label="나를 소개하는 글"
         help="나를 소개하는 글은 주로 프로필에 대한 상세 항목이 나오기 전 인삿말에 표현됩니다."
       >
         <UTextarea
           v-model="userProfile.intro"
-          placeholder="나를 소개하는 짧은 글을 입력하세요."
+          placeholder="나를 소개하는 글을 입력하세요."
           size="xl"
           :maxlength="100"
         />
--- src/components/templates/Header.vue
+++ src/components/templates/Header.vue
@@ -7,13 +7,13 @@ const { userProfileData } = storeToRefs(templateStore)
 
 <template>
   <header class="fixed top-0 left-0 w-full p-3 backdrop-blur">
-    <div class="flex justify-between items-center">
+    <!-- <div class="flex justify-between items-center">
       <div class="hidden lg:block"></div>
       <div>
         <p class="font-bold inline-block">{{ userProfileData?.name }}</p>
         {{ userProfileData?.position }}
       </div>
       <div></div>
-    </div>
+    </div> -->
   </header>
 </template>
