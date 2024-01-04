d4a2b0aa29e1e47e3d2350779016359a97d340d1
n2ptune <n2ptune@outlook.com>
2024-01-04 17:38:26 +0900

    fix: styling for standard-dark template

--- src/components/templates/standard/Hero.vue
+++ src/components/templates/standard/Hero.vue
@@ -2,11 +2,48 @@
 import { useTemplateStore } from '~/store/template'
 
 const templateStore = useTemplateStore()
-const { userProfileData } = storeToRefs(templateStore)
+const { userProfileData, templateTheme } = storeToRefs(templateStore)
+
+let io: IntersectionObserver
+
+onMounted(() => {
+  // Standard Dark 템플릿인 경우 hero 페이지가 보일 때와
+  // 보이지 않을 때 헤더의 스타일을 다르게 적용
+  if (templateTheme.value === 'dark') {
+    const hero = document.querySelector('.hero')
+    if (hero && 'IntersectionObserver' in window) {
+      io = new IntersectionObserver(
+        (entries) => {
+          const changeHeaderColor = (color: string) => {
+            const header = document.querySelector('header')
+            if (header) {
+              header.style.color = color
+            }
+          }
+          if (entries.every((entry) => entry.intersectionRatio <= 0)) {
+            changeHeaderColor('white')
+          } else {
+            changeHeaderColor('black')
+          }
+        },
+        { threshold: 0.001 }
+      )
+      io.observe(hero)
+    }
+  }
+})
+
+onBeforeUnmount(() => {
+  if (templateTheme.value === 'dark') {
+    io.disconnect()
+  }
+})
 </script>
 
 <template>
-  <div class="pt-12 pb-2 bg-primary-200 dark:bg-gray-900">
+  <div
+    class="hero pt-12 pb-4 bg-primary-200 dark:bg-primary-300 dark:text-black"
+  >
     <div class="container mx-auto my-6">
       <div class="flex flex-wrap justify-between">
         <div class="block">
