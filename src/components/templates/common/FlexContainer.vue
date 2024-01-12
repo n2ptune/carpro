97775f7a78ccaf164f36faace32ba46e5f2bffd0
n2ptune <n2ptune@outlook.com>
2024-01-12 15:10:05 +0900

    fix: add truncate class to new line overflowed contents to template `FlexContainer`

--- src/components/templates/common/FlexContainer.vue
+++ src/components/templates/common/FlexContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-wrap lg:flex-nowrap space-y-2 lg:space-y-0 lg:space-x-3"
+    class="flex flex-wrap lg:flex-nowrap space-y-2 lg:space-y-0 lg:space-x-3 truncate whitespace-pre-wrap"
   >
     <slot />
   </div>
