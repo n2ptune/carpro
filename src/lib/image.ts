206440b61f3c679afa3083705aba07ebafcf9149
n2ptune <n2ptune@outlook.com>
2024-01-15 16:44:12 +0900

    feat: add function that related to image

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,11 +1,17 @@
-import { getStorage, ref } from 'firebase/storage'
+import { deleteObject, getStorage, ref, uploadBytes } from 'firebase/storage'
 import { genUid } from './uid'
 
-export async function uploadImage(image: File, prefix = 'images/') {
-  const storage = getStorage()
-  const imageUid = genUid(20)
-  const path = prefix + imageUid
-  const imageRef = ref(storage, path)
+function getRef(path: string) {
+  return ref(getStorage(), path)
+}
 
-  // imageRef.
+export async function uploadImage(image: File, prefix = 'images/') {
+  const path = prefix + genUid(20)
+  const imageRef = getRef(path)
+  return await uploadBytes(imageRef, image)
+}
+
+export async function image(prefix: string, name: string) {
+  const imageRef = getRef(prefix + name)
+  return await deleteObject(imageRef)
 }
